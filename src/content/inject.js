eec62ef98540555f90a96c9a26908aa630b098ca
paul <paul.genho@gmail.com>
2019-04-11 15:42:34 -0600

    reset used time to 0 on new day

--- src/content/inject.js
+++ src/content/inject.js
@@ -1,28 +1,47 @@
-var content = chrome.extension.getURL('js/content.js')
+// var content = chrome.extension.getURL('js/content.js')
 // var storage = chrome.storage.local
-var script = document.createElement('script')
-script.setAttribute('type', 'text/javascript')
-script.setAttribute('src', content)
-document.body.appendChild(script);
+// var script = document.createElement('script')
+// script.setAttribute('type', 'text/javascript')
+// script.setAttribute('src', content)
+// document.body.appendChild(script);
+const moment = require('moment');
 
 (() => {
     let url = window.location.href
     url = url.replace(/^.*:\/\//i, '')
     url = url.split('/')[0]
-    // console.log(url)
+    console.log(url)
 
-    chrome.storage.local.get(url, (result) => {
-        if (!result[url]) {
-            // not an restricted page
-            return
-        }
-        console.log(result)
+    const monitor = () => {
+        chrome.storage.local.get(url, (storedEntry) => {
+            if (!storedEntry[url]) {
+                // not an restricted page
+                return
+            }
 
-        window.addEventListener('focus', () => {
-            console.log('start')
+            console.log(storedEntry)
+
+            //update date and minutes used
+            const resetTime = () => {
+                let updated = {}
+                let newLogDate = moment().format('YYYY-MM-DD')
+                updated[url] = {minutes: storedEntry.minutes, minutes_used: 0, last_log_date: newLogDate}
+                chrome.storage.local.set(updated, (r) => {
+                    monitor()
+                })
+            }
+
+            window.addEventListener('focus', () => {
+                let currentDate = moment().format('YYYY-MM-DD')
+
+                if (currentDate > storedEntry.last_log_date) {
+                    return resetTime()
+                }
+            })
+
+            window.addEventListener('blur', () => {
+                console.log('stop')
+            })
         })
-        window.addEventListener('blur', () => {
-            console.log('stop')
-        })
-    })
+    }
 })()
