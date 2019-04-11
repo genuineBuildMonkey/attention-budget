778a0e439f3fb9c136dda7864954be6c97fd68bc
paul <paul.genho@gmail.com>
2019-04-11 16:53:24 -0600

    cycle timer and log time

--- src/content/inject.js
+++ src/content/inject.js
@@ -12,36 +12,72 @@ const moment = require('moment');
     url = url.split('/')[0]
     console.log(url)
 
-    const monitor = () => {
+    const monitor = (reset) => {
         chrome.storage.local.get(url, (storedEntry) => {
             if (!storedEntry[url]) {
-                // not an restricted page
+                // not a restricted page
                 return
             }
 
-            console.log(storedEntry)
+            var intervalTimer
 
             //update date and minutes used
-            const resetTime = () => {
+            const resetTime = (newLogDate) => {
                 let updated = {}
-                let newLogDate = moment().format('YYYY-MM-DD')
-                updated[url] = {minutes: storedEntry.minutes, minutes_used: 0, last_log_date: newLogDate}
+                //let newLogDate = moment().format('YYYY-MM-DD')
+                updated[url] = {minutes: storedEntry[url].minutes, minutes_used: 0, last_log_date: newLogDate}
                 chrome.storage.local.set(updated, (r) => {
-                    monitor()
+                    // restart monitor after updating date and available time
+                    monitor(true)
                 })
             }
 
-            window.addEventListener('focus', () => {
+            const startMonitor = () => {
                 let currentDate = moment().format('YYYY-MM-DD')
-
-                if (currentDate > storedEntry.last_log_date) {
-                    return resetTime()
+                if (currentDate > storedEntry[url].last_log_date) {
+                    return resetTime(currentDate)
                 }
+                try {
+                    clearInterval(intervalTimer)
+                } catch (e) {
+                }
+
+                intervalTimer = setInterval(() => {
+                    //15 seconds on to used time
+                    storedEntry[url].minutes_used += 0.25
+
+                    let updated = {}
+
+                    updated[url] = {
+                        minutes: storedEntry[url].minutes,
+                        minutes_used: storedEntry[url].minutes_used,
+                        last_log_date: storedEntry[url].last_log_date}
+
+                    try {
+                        chrome.storage.local.set(updated, (r) => {
+                            console.log('updated', updated)
+                        })
+                    } catch (e) {
+                        console.log(e)
+                    }
+                }, 15000)
+            }
+
+            window.addEventListener('focus', () => {
+                console.log('start')
+                startMonitor()
             })
 
             window.addEventListener('blur', () => {
                 console.log('stop')
+                clearInterval(intervalTimer)
             })
+
+            if (reset) {
+                startMonitor()
+            }
         })
     }
+
+    monitor()
 })()
